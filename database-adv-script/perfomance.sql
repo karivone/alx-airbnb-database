85ad8553a99675c657f9d94a8e0fdb92d0ea3e68
karivone <karimiyvonne19@gmail.com>
2025-05-12 15:49:49 +0300

    SQL QUERIES

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -4,8 +4,12 @@ FROM Booking b
 JOIN User u ON b.user_id = u.user_id
 JOIN Property p ON b.property_id = p.property_id
 LEFT JOIN Payment pay ON b.booking_id = pay.booking_id;
+WHERE b.status = 'confirmed'
+AND b.start_date >= '2024-06-01';
+
 
 -- Refactored Query (reduced columns and using indexes)
+EXPLAIN ANALYZE
 SELECT b.booking_id, b.start_date, b.end_date, b.status,
        u.first_name, u.last_name,
        p.name AS property_name,
@@ -14,3 +18,5 @@ FROM Booking b
 JOIN User u ON b.user_id = u.user_id
 JOIN Property p ON b.property_id = p.property_id
 LEFT JOIN Payment pay ON pay.booking_id = b.booking_id;
+WHERE b.status = 'confirmed'
+AND b.start_date >= '2024-06-01';
